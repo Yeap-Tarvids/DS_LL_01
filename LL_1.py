07003282a7966588447f2e6ff8460bacd43126e6
Yeap-Tarvids <Ricards.Tarvids@edu.rtu.lv>
2025-02-17 20:00:57 +0200

    Update LL_1.py
    241RDB204 | Pievienoju find_middle_node metodi un pie print pierakstīju "middle node:"

--- LL_1.py
+++ LL_1.py
@@ -20,15 +20,14 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         return True
-        
 
-    # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
-
+    
+    def find_middle_node(self):
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
 
 
 my_linked_list = LinkedList(1)
@@ -37,5 +36,4 @@ my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
-
+print("middle node:", my_linked_list.find_middle_node().value)
